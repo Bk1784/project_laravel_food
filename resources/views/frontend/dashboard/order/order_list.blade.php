13ea25fecbb718625b67423a8a0d0f5c935da78f
bk1784 <futonabuu456@gmail.com>
2024-12-03 16:10:23 +0700

    memanajemen order user panel 2

--- resources/views/frontend/dashboard/order/order_list.blade.php
+++ resources/views/frontend/dashboard/order/order_list.blade.php
@@ -56,7 +56,8 @@
                    
                     
             <td class="d-flex">
-             <a href="{{ route('admin.order.details',$item->id) }}" class="btn-small d-block text-primary"> <i class="fas fa-eye"></i> View</a> 
+            <td class="d-flex justify-content-between">
+            <a href="{{ route('user.order.details',$item->id) }}" class="btn-small d-block text-primary"> <i class="fas fa-eye"></i> View</a> 
             <a href="{{ route('admin.order.details',$item->id) }}" class="btn-small d-block text-danger"> <i class="fa fa-download"></i> Invoice</a>
     
                     </td> 
