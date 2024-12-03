c69ae3d26d1f7389c443b511998f7b824d071647
bk1784 <futonabuu456@gmail.com>
2024-12-03 08:28:37 +0700

    mengaplikasikan cash on delivery 3

--- app/Http/Controllers/Frontend/OrderController.php
+++ app/Http/Controllers/Frontend/OrderController.php
@@ -3,12 +3,51 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Auth;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Request;
+use Session;
 
 class OrderController extends Controller
 {
-    public function CashOrder(){
-    }
+    public function CashOrder(Request $request){
+        $validateData = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+        ]);
+        $cart = session()->get('cart',[]);
+        $totalAmount = 0;
+        foreach($cart as $car){
+            $totalAmount += ($cart['price'] * $cart['quantity']);
+        }
+        if (Session->has('coupon')) {
+            $tt = (Session()->get('coupon')['discount_amount']);
+        } else {
+            $tt = $totalAmount;
+        }
+        
+        $order_id = Order::insertGetId([
+            'user_id' => Auth::id(),
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'payment_type' => 'Cash On Delivery',
+            'payment_method' => 'Cash On Delivery',
+            'currency' => 'USD',
+            'amount' => $totalAmount,
+            'total_amount' => $tt,
+            'invoice_no' => 'easyshop' .mt_rand(10000000,99999999),
+            'order_date' => Carbon::now()->format('d F Y'),
+            'order_month' => Carbon::now()->format('F'),
+            'order_year' => Carbon::now()->format('Y'),
+            'status' => 'Pending',
+            'created_at' => Carbon::now(), 
+        ]);
     //End Method 
+}
 }
