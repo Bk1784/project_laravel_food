466b5c6fc54a767e34c190804276386f422ac286
bk1784 <futonabuu456@gmail.com>
2024-12-08 23:09:31 +0700

    memanajemen perlaporan sistem admin bagian 3

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use DateTime;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -10,4 +12,13 @@ class ReportController extends Controller
     public function AdminAllReports(){
         return view('admin.backend.report.all_report');
     }
+
+    public function AminSearchByDate(Request $request){
+        $date = new DateTime($request->date);
+        $formatDate = $date->format('d F Y');
+
+        $orderDate = Order::where('order_date',$formatDate)->latest()->get();
+        return view('admin.backend.report.search_by_date',compact('orderDate','formatDate'));
+    }
+      // End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -143,6 +143,8 @@ Route::middleware('admin')->group(function () {
     });
     Route::controller(ReportController::class)->group(function(){
         Route::get('/admin/all/reports', 'AdminAllReports')->name('admin.all.reports'); 
+        Route::post('/admin/search/bydate', 'AminSearchByDate')->name('admin.search.bydate');
+    
     });
     
 });
